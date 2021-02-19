0c34b586bfc4e5d75927d4d4cfbb0ad15b7109eb
Hannah Hertzog <hhertzog@gmail.com>
2021-02-19 15:33:58 +0900

    making page for posts from other users that match my keywords

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :posts
   #get 'home/index'
   get 'home/about'
+  get 'home/keyword_posts'
   root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
